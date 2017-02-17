f739e6cad1b04c6e0cea51b70fee54370546eb88
rajeev12312 <rajeev.orcl@gmail.com>
2017-02-16 23:56:27 -0600

    Update ValidatorSpockTests.groovy

--- src/test/groovy/org/springframework/samples/petclinic/model/ValidatorSpockTests.groovy
+++ src/test/groovy/org/springframework/samples/petclinic/model/ValidatorSpockTests.groovy
@@ -44,12 +44,60 @@ public class ValidatorSpockTests extends Specification {
 	}
 	
 	// 2. TODO: validate against null first name.
+	def "first name cannot be null"() {
+	  setup:
+	  def person1 = new Person();
+	  def validator = createValidator();
+	  
+	  when:
+	  person1.setFirstName(null)
+	  person1.setLastName("smith")
+      Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person1);
+	  
+	  then:
+      constraintViolations.size() == 1;
+      ConstraintViolation<Person> violation = constraintViolations.iterator().next();
+	  violation.getPropertyPath().toString().equals("firstName");
+	  violation.getMessage().equals("may not be empty");
+	}
 	
-	// 3. TODO: validate a against a valid first name (non-empty, non-null value).
-
-	// 4. TODO: validate against empty last name
+// 4. TODO: validate against empty last name
+	
+	def "last name cannot be empty"() {
+	  setup:
+	  def person = new Person();
+	  def validator = createValidator();
+	  
+	  when:
+	  person.setFirstName("rajeev")
+	  person.setLastName("")
+      Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
+	  
+	  then:
+      constraintViolations.size() == 1;
+      ConstraintViolation<Person> violation = constraintViolations.iterator().next();
+	  violation.getPropertyPath().toString().equals("lastName");
+	  violation.getMessage().equals("may not be empty");
+	}
 
 	// 5. TODO: validate against null last name
 	
-	// 6. TODO: validate a against a valid last name (non-empty, non-null value).
+	def "last name cannot be null"() {
+	  setup:
+	  def person = new Person();
+	  def validator = createValidator();
+	  
+	  when:
+	  person.setFirstName("rajeev")
+	  person.setLastName(null)
+      Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
+	  
+	  then:
+      constraintViolations.size() == 1;
+      ConstraintViolation<Person> violation = constraintViolations.iterator().next();
+	  violation.getPropertyPath().toString().equals("lastName");
+	  violation.getMessage().equals("may not be empty");
+	}
+
+	
 }
